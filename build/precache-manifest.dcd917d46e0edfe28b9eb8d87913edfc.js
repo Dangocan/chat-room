2620bbf7814620f99c07b2bf91ca58619113028b
Dangocan <danilogc1999@gmail.com>
2020-10-18 15:42:29 -0300

    Teste de build

---
+++ build/precache-manifest.dcd917d46e0edfe28b9eb8d87913edfc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea62095d412473eb8c56ed0952dd7241",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85fe31de88fc09eafc25",
+    "url": "/static/js/2.0edd4320.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.0edd4320.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a49c8c2063901f2855a",
+    "url": "/static/js/main.66951937.chunk.js"
+  },
+  {
+    "revision": "02431dfc46b843c90051",
+    "url": "/static/js/runtime-main.3a6df2d0.js"
+  }
+]);
--- server_express/server.js
+++ server_express/server.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import socket from 'socket.io';
 import http from 'http';
+import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
+});
 
 server.listen(8080, () => console.log('Servidor rodando na porta 8080'));
 
